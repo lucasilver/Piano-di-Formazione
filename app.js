bdb17b99b7a8cce24c591851884850dcb39c3ef3
lucasilver <versinoluca@gmail.com>
2026-07-21 09:23:31 +0200

    Update app.js
    elimino <small>"segmento formativo"</small>

--- app.js
+++ app.js
@@ -206,7 +206,7 @@ function renderTabella(listaCorsi) {
         <tr>
             <td><strong>${c.lepta}</strong></td>
             <td>${c.area}</td>
-            <td><small>${c.segmento_formativo}</small></td>
+            <td>${c.segmento_formativo}</td>
             <td><strong>${c.argomento}</strong></td>
             <td>${c.valenza}</td>
             <td>${c.tipologia}</td>
